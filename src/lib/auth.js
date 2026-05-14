0518d0aea49f030b8d9a4c44355c528da8f2bd79
puc-void <215346186+puc-void@users.noreply.github.com>
2026-05-14 22:16:31 +0600

    auth fixed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,6 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "@/lib/prisma";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export const authOptions = {
   providers: [
